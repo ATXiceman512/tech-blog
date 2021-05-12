a1cfbaaa758b63eb5f0cdc2890737f880d77baf5
jbeard <player69>
2021-05-11 22:17:30 -0700

    added connection.js , schema.sql, and server.js files

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const curSession = {
+  secret: process.env.EXPRESS_SECRET,
+  cookie: {
+    maxAge: 300000
+  },
+  resave: false,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(curSession));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Program now running at: http://localhost:${PORT}`))
+})
